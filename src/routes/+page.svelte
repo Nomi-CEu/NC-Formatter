48c66bbba7faddcb8c102a11041f243d3e730324
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2025-03-05 11:26:21 +1100

    Padding on Bottom, Improve Color Scheme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
   import Data from "$lib/Data.svelte";
 </script>
 
-<main class="text-text m-auto max-w-4xl">
+<main class="text-text m-auto max-w-4xl pb-10">
   <h1 class="mx-auto mt-8 mb-4 text-center text-4xl font-bold">Nomi-CEu NuclearCraft Formatter</h1>
   <p class="subtitle text-lg">
     Tool to convert Reactor JSON Files to Building Gadgets Strings, for Nomi-CEu!
